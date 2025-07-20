5542d6a613932ef0286f43b67e83964f7d7ebe63
dalvarez-umg <dalvareze@miumg.edu.gt>
2025-07-20 11:42:30 -0600

    prueba clase
    prueba borrado

--- Clase2/src/main/java/Suma.java
+++
@@ -1,9 +0,0 @@
-public class Suma {
-    public static void main(String[] args) {
-        int a = 10;         // Primer número
-        int b = 5;          // Segundo número
-        int suma = a + b;   // Operador de suma
-
-        System.out.println("La suma es: " + suma);
-    }
-}
