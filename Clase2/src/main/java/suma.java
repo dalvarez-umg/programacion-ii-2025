2d05fe10b6dfbac6a71f5d09c6674653f31369f6
dalvarez-umg <dalvareze@miumg.edu.gt>
2025-07-22 12:32:56 -0600

    Clase 2
    Clase 2 de introducción a JAVA

---
+++ Clase2/src/main/java/suma.java
@@ -0,0 +1,9 @@
+public class suma {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 20;
+        int suma = numero1 + numero2;
+        
+        System.out.println(suma);
+    }
+}
